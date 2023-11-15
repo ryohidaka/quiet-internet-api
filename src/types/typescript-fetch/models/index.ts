ce18a3c906cb2c88b6e065eb420f8c43f7dfe5a0
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2023-11-15 22:24:01 +0900

    feat: Generate quiet-internet-api types.

---
+++ src/types/typescript-fetch/models/index.ts
@@ -0,0 +1,7 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './ApiV1PostsGet200Response';
+export * from './ApiV1PostsSlugGet200Response';
+export * from './Pagination';
+export * from './PostDetail';
+export * from './PostMeta';
